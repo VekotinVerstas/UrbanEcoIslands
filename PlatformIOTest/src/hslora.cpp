a2a15151f4e4f6bebd6ef182b45fff2bc6433686
Aki Salminen <code@salminen.info>
2019-10-18 21:38:41 +0300

    Revert back to os sheduler for stable loraOs sheduler might be temporary solution or it could be used to shehule other tasks too.

--- PlatformIOTest/src/hslora.cpp
+++ PlatformIOTest/src/hslora.cpp
@@ -12,19 +12,87 @@
 // left empty here (we cannot leave them out completely unless
 // DISABLE_JOIN is set in config.h, otherwise the linker will complain).
 void os_getArtEui(u1_t *buf) {}
-void os_getDevEui(u1_t *buf) {}
+void os_getDevEui(u1_t* buf) {
+  //memcpy_P(buf, DEVEUI, 8);
+}
 void os_getDevKey(u1_t *buf) {}
 
-void onEvent(ev_t ev)
-{
-  if (ev == EV_TXCOMPLETE)
-  {
-    // Schedule nexvt transmission
-    //os_setTimedCallback(&sendjob, os_getTime() + sec2osticks(TX_INTERVAL), do_send);
-    Serial.printf("LoRa EV_TXCOMPLETE, next send in %d seconds\n", TX_INTERVAL);
+void do_send(osjob_t* j) {
+  // Check if there is not a current TX/RX job running
+  if (LMIC.opmode & OP_TXRXPEND) {
+    Serial.println(F("OP_TXRXPEND, not sending"));
+  } else {
+    // Prepare upstream data transmission at the next possible time.
+    unsigned char text[]={"test"};
+    LMIC_setTxData2(1, (xref2u1_t)text, 5, 0);
+    Serial.println("do_send viesti j0nossa");
+  }
+  // Next TX is scheduled after TX_COMPLETE event.
+}
 
-    schedule_next_task_run(send_data_lora, TX_INTERVAL, false);
-    clear_to_sleep = true; // buffer clear, sleep ok
+void onEvent (ev_t ev) {
+  Serial.print(os_getTime());
+  Serial.print(": ");
+  switch (ev) {
+    case EV_SCAN_TIMEOUT:
+      Serial.println(F("EV_SCAN_TIMEOUT"));
+      break;
+    case EV_BEACON_FOUND:
+      Serial.println(F("EV_BEACON_FOUND"));
+      break;
+    case EV_BEACON_MISSED:
+      Serial.println(F("EV_BEACON_MISSED"));
+      break;
+    case EV_BEACON_TRACKED:
+      Serial.println(F("EV_BEACON_TRACKED"));
+      break;
+    case EV_JOINING:
+      Serial.println(F("EV_JOINING"));
+      break;
+    case EV_JOINED:
+      Serial.println(F("EV_JOINED"));
+      break;
+    case EV_JOIN_FAILED:
+      Serial.println(F("EV_JOIN_FAILED"));
+      break;
+    case EV_REJOIN_FAILED:
+      Serial.println(F("EV_REJOIN_FAILED"));
+      break;
+    case EV_TXCOMPLETE:
+      Serial.println(F("EV_TXCOMPLETE (includes waiting for RX windows)"));
+      if (LMIC.txrxFlags & TXRX_ACK)
+        Serial.println(F("Received ack"));
+      if (LMIC.dataLen) {
+        Serial.print(F("Received "));
+        Serial.print(LMIC.dataLen);
+        Serial.println(F(" bytes of payload"));
+      }
+      // Schedule next transmission
+      Serial.printf("Next LoRa send in %d seconds\n", TX_INTERVAL);
+      //schedule_next_task_run(send_data_lora, TX_INTERVAL, true);
+      os_setTimedCallback(&sendjob, os_getTime() + sec2osticks(TX_INTERVAL), do_send);
+      clear_to_sleep = true; // buffer clear, sleep ok
+      break;
+    case EV_LOST_TSYNC:
+      Serial.println(F("EV_LOST_TSYNC"));
+      break;
+    case EV_RESET:
+      Serial.println(F("EV_RESET"));
+      break;
+    case EV_RXCOMPLETE:
+      // data received in ping slot
+      Serial.println(F("EV_RXCOMPLETE"));
+      break;
+    case EV_LINK_DEAD:
+      Serial.println(F("EV_LINK_DEAD"));
+      break;
+    case EV_LINK_ALIVE:
+      Serial.println(F("EV_LINK_ALIVE"));
+      break;
+    default:
+      Serial.print(F("Unknown event: "));
+      Serial.println((unsigned) ev);
+      break;
   }
 }
 #endif //SEND_DATA_LORA_2_ENABLED
@@ -68,6 +136,8 @@ void hslora_setup()
 
         // Start job
         //do_send(&sendjob);
+        //schedule_next_task_run(send_data_lora, TX_INTERVAL, false);
+        os_setTimedCallback(&sendjob, os_getTime() + sec2osticks(TX_INTERVAL), do_send);
     }
     #endif //SEND_DATA_LORA_2_ENABLED
 }
